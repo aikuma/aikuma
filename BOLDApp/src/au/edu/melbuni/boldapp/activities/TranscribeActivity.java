0a76b8c0d5613658362bc1cd473223346ce7625f
Florian Hanke <florian.hanke@gmail.com>
2012-02-23 09:02:19 +1000

    + respeaking got its own activity

---
+++ BOLDApp/src/au/edu/melbuni/boldapp/activities/TranscribeActivity.java
@@ -0,0 +1,66 @@
+package au.edu.melbuni.boldapp.activities;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff.Mode;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import au.edu.melbuni.boldapp.R;
+import au.edu.melbuni.boldapp.Sounder;
+import au.edu.melbuni.boldapp.SpeechController;
+import au.edu.melbuni.boldapp.Transcriber;
+import au.edu.melbuni.boldapp.listeners.OnCompletionListener;
+import au.edu.melbuni.boldapp.persisters.Persister;
+
+public class TranscribeActivity extends BoldActivity {
+
+	boolean listening = false;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		configureView(savedInstanceState);
+		installBehavior(savedInstanceState);
+	}
+
+	@Override
+	public void configureView(Bundle savedInstanceState) {
+		super.configureView(savedInstanceState);
+
+		setContent(R.layout.respeak);
+	};
+
+	public void installBehavior(Bundle savedInstanceState) {
+		final ImageButton transcribeButton = (ImageButton) findViewById(R.id.respeakButton);
+		final SpeechController transcriber = new Transcriber();
+		transcribeButton.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View view) {
+				if (!listening) {
+					transcriber.listen(
+							Persister.getBasePath()
+									+ "timelines/3711a772-078c-4cfe-a2ab-4d4a7dce2d06/segments/0",
+							new OnCompletionListener() {
+
+								@Override
+								public void onCompletion(Sounder sounder) {
+									listening = false;
+									transcribeButton.getBackground()
+											.clearColorFilter();
+								}
+
+							});
+					transcribeButton.getBackground().setColorFilter(Color.GREEN,
+							Mode.MULTIPLY);
+					listening = true;
+				} else {
+					transcriber.stop();
+					listening = false;
+					transcribeButton.getBackground().clearColorFilter();
+				}
+				return false;
+			}
+		});
+	}
+}
