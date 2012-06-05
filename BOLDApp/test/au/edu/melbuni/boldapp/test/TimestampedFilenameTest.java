03e507a3b4d333b0cc075182583c3ea87c712c23
Florian Hanke <florian.hanke@gmail.com>
2012-06-05 13:27:29 +1000

    + TimestampedFilenameTest

--- BOLDApp/test/au/edu/melbuni/boldapp/test/TimestampedFilenameTest.java
+++ BOLDApp/test/au/edu/melbuni/boldapp/test/TimestampedFilenameTest.java
@@ -1,6 +1,6 @@
 package au.edu.melbuni.boldapp.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +9,17 @@ import au.edu.melbuni.boldapp.TimestampedFilename;
 
 @RunWith(CustomTestRunner.class)
 public class TimestampedFilenameTest {
-
-//	@Test
-//	public void isCorrect() {
-//		assertEquals("hello-3547799261.wav",
-//				TimestampedFilename.getFilenameFor("hello.wav"));
-//	}
+	
+	@Test
+	public void worksWithExtensionsWithDigits() {
+		assertTrue(TimestampedFilename.getFilenameFor("hello.123").matches(
+				"hello-\\d{10,}\\.123"));
+	}
+	
+	@Test
+	public void hasTenOrMoreDigits() {
+		assertTrue(TimestampedFilename.getFilenameFor("hello.wav").matches(
+				"hello-\\d{10,}\\.wav"));
+	}
 
 }
