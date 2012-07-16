568d386b6c1cb4b42f6c6459fb32ff76567e0e2d
Oliver Adams <oliver.adams@gmail.com>
2012-07-16 10:56:23 +1000

    added record activity and the ability to move to that activity.

---
+++ BOLDApp/src/au/edu/unimelb/boldapp/RecordActivity.java
@@ -0,0 +1,12 @@
+package au.edu.unimelb.boldapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.record);
+	}
+}
