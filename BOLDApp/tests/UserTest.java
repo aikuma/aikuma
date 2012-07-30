2a231439d231a16ccf196499f4d38f9ca4a51371
Oliver Adams <oliver.adams@gmail.com>
2012-07-30 15:17:47 +1000

    Now with running tests

---
+++ BOLDApp/tests/UserTest.java
@@ -0,0 +1,27 @@
+package au.edu.unimelb.boldapp.test;
+
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
+
+import au.edu.unimelb.boldapp.User;
+
+//@RunWith(CustomTestRunner.class)
+public class UserTest {
+	private User user;
+
+	@Before
+	public void setUp() throws Exception {
+		this.user = new User(
+				UUID.fromString("f47ac10b-58cc-4372-a567-0e02b2c3d479"),
+				"Some name");
+	}
+
+	@Test
+	public void getName() {
+		assertEquals("Some name", user.getName());
+	}
+}
