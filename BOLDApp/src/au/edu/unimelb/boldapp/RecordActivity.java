a718bc5bf906c66d2e573916c57dacedf12e3324
Oliver Adams <oliver.adams@gmail.com>
2012-07-31 11:06:12 +1000

    Implemented functionality for the buttons in Record activity. UserIcon goes to UserSelectionActivity, and the record button toggles a boolean

--- BOLDApp/src/au/edu/unimelb/boldapp/RecordActivity.java
+++ BOLDApp/src/au/edu/unimelb/boldapp/RecordActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
+import android.content.Intent;
 
 public class RecordActivity extends Activity {
+	private Boolean recording;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.record);
+		recording = false;
 	}
 
 	@Override
@@ -19,5 +22,16 @@ public class RecordActivity extends Activity {
 
 	public void goBack(View view){
 		RecordActivity.this.finish();
+	}
+
+	public void goToUserSelection(View view){
+		Intent intent = new Intent(this, UserSelectionActivity.class);
+		startActivity(intent);
+	}
+
+	public void record(View view) {
+		//Toggle the recording Boolean.
+		recording = !recording;
+		Log.i("yoyoyo", recording.toString());
 	}
 }
