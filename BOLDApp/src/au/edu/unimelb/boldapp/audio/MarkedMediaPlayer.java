10592a1706ad98e6f30e687bf92beb85b791588b
Oliver Adams <oliver.adams@gmail.com>
2012-10-04 17:02:53 +1000

    added the MarkedMediaPlayer class that extends MediaPlayer

---
+++ BOLDApp/src/au/edu/unimelb/boldapp/audio/MarkedMediaPlayer.java
@@ -0,0 +1,101 @@
+package au.edu.unimelb.boldapp.audio;
+
+import android.util.Log;
+
+import android.media.MediaPlayer;
+
+/**
+ * Extends android.media.MediaPlayer to allow for the use of notification
+ * markers in the same style as used by android.Media.AudioTrack.
+ *
+ * @author	Oliver Adams	<oliver.adams@gmail.com>
+ * @author	Florian hanke	<florian.hanke@gmail.com>
+ */
+public class MarkedMediaPlayer extends MediaPlayer {
+	/**
+	 * The marker that determines when the playbackPositionUpdateListener gets
+	 * called.
+	 */
+	private int notificationMarkerPosition;
+
+	/**
+	 * The listener the MarkedMediaPlayer notifies when a previously set marker
+	 * is reached.
+	 */
+	private OnMarkerReachedListener onMarkerReachedListener;
+
+	/**
+	 * Returns the marker position in milliseconds
+	 *
+	 * @return	marker position in milliseconds
+	 */
+	public int getNotificationMarkerPosition() {
+		return notificationMarkerPosition;
+	}
+
+	/**
+	 * Sets the position of the notification marker.
+	 *
+	 * @param	notificationMarkerPosition	marker in milliseconds
+	 */
+	public void setNotificationMarkerPosition(int notificationMarkerPosition) {
+		this.notificationMarkerPosition = notificationMarkerPosition;
+	}
+
+	/**
+	 * Sets the listener the MarkedMediaPlayer notifies when a previously set
+	 * marker is reached.
+	 *
+	 * @param	onMarkerReachedListener	the listener to be notified.
+	 */
+	public void setOnMarkerReachedListener( OnMarkerReachedListener
+			onMarkerReachedListener) {
+		this.onMarkerReachedListener = onMarkerReachedListener;
+	}
+
+	/**
+	 * The class for listeners what would be called when set markers get
+	 * reached.
+	 */
+	public static abstract class OnMarkerReachedListener {
+		public abstract void onMarkerReached(MarkedMediaPlayer p);
+	}
+
+	/**
+	 * Implements a run that polls the current position to check
+	 * if a previously set marker has been reache
+	 */
+	private class NotificationMarkerLoop implements Runnable {
+		private int position;
+
+		public void run() {
+			while (getCurrentPosition() < getDuration()) {
+				Log.i("asdf", " " + getCurrentPosition());
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// Would be a programming error
+				}
+				if (getCurrentPosition() >= getNotificationMarkerPosition()) {
+					if(onMarkerReachedListener != null) {
+						onMarkerReachedListener.onMarkerReached(
+								MarkedMediaPlayer.this);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Standard constructor that starts a NotificationMarkerLoop once the
+	 * MarkedMediaPlayer is prepared.
+	 */
+	public MarkedMediaPlayer() {
+		super();
+		setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+			public void onPrepared(MediaPlayer _) {
+				new Thread(new NotificationMarkerLoop()).start();
+			}
+		});
+	}
+}
