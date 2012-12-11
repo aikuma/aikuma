6db5f1d7fa365fc26eef4bbc694275911490f4de
Oliver Adams <oliver.adams@gmail.com>
2012-12-12 10:06:26 +1100

    got a prototype language list to appear when one hits the interpret button

--- BOLDApp/src/au/edu/unimelb/boldapp/FilterList.java
+++ BOLDApp/src/au/edu/unimelb/boldapp/FilterList.java
@@ -4,31 +4,81 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import com.google.common.base.Charsets;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import org.apache.commons.io.IOUtils;
 
 public class FilterList extends ListActivity {
 
 	private EditText filterText = null;
 	private ArrayAdapter<String> adapter = null;
-	
+	private Map langCodeMap;
+
+	/**
+	 * Loads the language codes from the text file resource and makes a mapping
+	 * from language code to language name.
+	 */
+	public void loadLangCodes() throws IOException {
+		/*
+		InputStream is = getResources().openRawResource(R.raw.iso_639_3);
+		StringWriter writer = new StringWriter();
+		String inputString = writer.toString();
+		langCodeMap = new HashMap<String,String>();
+		String[] lines = inputString.split("\n");
+		for (String line : lines) {
+			String[] elements = line.split("(?=\t)");
+			Log.i("langcodes", line);
+			Log.i("langcodes", " " + elements[0] + " " + elements[6]);
+			langCodeMap.put(elements[0].trim(), elements[6].trim());
+		}
+		*/
+		InputStream is = getResources().openRawResource(
+				R.raw.iso_639_3);
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(is, writer, Charsets.UTF_8);
+		String inputString = writer.toString();
+		Map<String,String> map = new HashMap<String,String>();
+		String[] lines = inputString.split("\n");
+		for (String line : lines) {
+			String[] elements = line.split("(?=\t)");
+			map.put(elements[6].trim(), elements[0].trim());
+		}
+		langCodeMap = map;
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.filter_list);
 		List list = new ArrayList<String>();
-		list.add("once");
-		list.add("upon");
-		list.add("a");
-		list.add("time");
-		filterText = (EditText) findViewById(R.id.search_box);
-		filterText.addTextChangedListener(filterTextWatcher);
-		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1,
-				list);
-		setListAdapter(adapter);
+		try {
+			loadLangCodes();
+			filterText = (EditText) findViewById(R.id.search_box);
+			filterText.addTextChangedListener(filterTextWatcher);
+			//Log.i("sick", " " + new ArrayList<String>(langCodeMap.values()).size() + 
+			//		new ArrayList<String>(new HashSet<String>(langCodeMap.values())).size());
+			List<String> l = new ArrayList<String>(langCodeMap.keySet());
+			adapter = new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1,
+					l);
+			setListAdapter(adapter);
+		} catch (IOException e) {
+			//Toast something here
+		}
+		//list.add("once");
+		//list.add("upon");
+		//list.add("a");
+		//list.add("time");
 	}
 
 	private TextWatcher filterTextWatcher = new TextWatcher() {
--- BOLDApp/tests/src/au/edu/unimelb/boldapp/LangCodeTest.java
+++ BOLDApp/tests/src/au/edu/unimelb/boldapp/LangCodeTest.java
@@ -1,6 +1,6 @@
 package au.edu.unimelb.boldapp;
 
-import android.test.AndroidTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
 import com.google.common.base.Charsets;
 import java.io.InputStream;
@@ -9,23 +9,21 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.io.IOUtils;
 
-public class LangCodeTest extends AndroidTestCase {
+public class LangCodeTest extends ActivityInstrumentationTestCase2<FilterList> {
+
+	private FilterList mActivity;
+
+	public LangCodeTest() {
+		super("au.edu.unimelb.boldapp", FilterList.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		mActivity = getActivity();
+	}
 
 	public void testLoad() throws Exception {
-		//AssetManager am = context.getAssets();
-		//InputStream is = am.open("iso-639-3_20120816.tab");
-		InputStream is = getContext().getResources().openRawResource(
-				R.raw.iso_639_3);
-		StringWriter writer = new StringWriter();
-			IOUtils.copy(is, writer, Charsets.UTF_8);
-		String inputString = writer.toString();
-		Map<String,String> map = new HashMap<String,String>();
-		String[] lines = inputString.split("\n");
-		for (String line : lines) {
-			String[] elements = line.split("(?=\t)");
-			map.put(elements[0].trim(), elements[6].trim());
-		}
-		Log.i("text", map.get("aaa"));
+		mActivity.loadLangCodes();
 	}
 
 }
