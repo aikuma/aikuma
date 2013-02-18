45d686cb8f97a2f32cc4331f1b940faec733a778
Oliver Adams <oliver.adams@gmail.com>
2013-02-18 12:13:39 +1100

    migrating User and corresponding unit tests. Adding readUser writeUser encodeUser etc methods that used to be in FileIO

---
+++ Aikuma/src/au/edu/unimelb/aikuma/model/User.java
@@ -0,0 +1,124 @@
+package au.edu.unimelb.aikuma.model;
+
+import android.graphics.Bitmap;
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * The class that contains user data.
+ *
+ * @author	Oliver Adams	<oliver.adams@gmail.com>
+ * @author	Florian Hanke	<florian.hanke@gmail.com>
+ */
+public class User {
+
+	/**
+	 * The user's UUID.
+	 */
+	private UUID uuid;
+	/**
+	 * The user's name.
+	 */
+	private String name;
+
+	/**
+	 * The user's languages
+	 */
+	private List<Language> languages;
+
+	/**
+	 * Constructor that allows for languages to be specified.
+	 *
+	 * @param	uuid	The new user's UUID.
+	 * @param	name	The new user's name.
+	 */
+	public User(UUID uuid, String name, List<Language> languages) {
+		setUuid(uuid);
+		setName(name);
+		setLanguages(languages);
+	}
+
+	/**
+	 * Languages accessor.
+	 */
+	public List<Language> getLanguages() {
+		return languages;
+	}
+
+	/**
+	 * Returns the small version of the image associated with a user. We don't
+	 * return a drawable because creating a BitmapDrawable requires access to
+	 * Resources.
+	 *
+	 * @return 	small version of a user's image; null if it doesn't exist.
+	 */
+	public Bitmap getSmallImage() {
+		return ImageUtils.retrieveFromFile(
+				new File(FileIO.getImagesPath(), getUUID() + ".small.jpg"));
+	}
+
+	/**
+	 * Languages mutator.
+	 */
+	public void setLanguages(List<Language> languages) { 
+		this.languages = languages;
+	}
+
+	/**
+	 * uuid mutator
+	 */
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	/**
+	 * name mutator
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * uuid accessor
+	 */
+	public UUID getUuid() {
+		return this.uuid;
+	}
+
+	/**
+	 * uuid accessor
+	 */
+	public UUID getUUID() {
+		return this.uuid;
+	}
+
+	/**
+	 * name accessor
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	public JSONObject encodeUser(User user) {
+		JSONObject encodedUser = new JSONObject();
+	}
+
+	/**
+	 * Write the user to file
+	 */
+	public void writeUser() throws IOException {
+	}
+
+	/**
+	 * Read a user from the file
+	 */
+	//public static User readUser(UUID uuid) {
+	//}
+
+	/**
+	 * Read all users from file
+	 */
+	//public static User readUser(UUID uuid) {
+	//}
+}
---
+++ Aikuma/test/src/au/edu/unimelb/aikuma/model/UserTest.java
@@ -0,0 +1,40 @@
+package au.edu.unimelb.aikuma.model;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+	protected void setUp() {
+		
+	}
+
+	/**
+	 * Test when the user has no associated data
+	 */
+	public void testWriteUser1() {
+		User user = new User();
+	}
+
+	/**
+	 * Test when the user just has a name
+	 */
+	public void testWriteUser2() {
+		User user = new User();
+		String name = "TestUser";
+		user.setName(name);
+	}
+
+	public void testWriteUser3() {
+		User user = new User();
+		UUID uuid = UUID.randomUUID();
+		user.setUUID(uuid);
+	}
+
+	public void testWriteUser4() {
+		User user = new User();
+		String name = "TestUser";
+		UUID uuid = UUID.randomUUID();
+		user.setName(name);
+		user.setUUID(uuid);
+	}
+}
