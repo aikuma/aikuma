732adbb4d81151af8c8a8a3420d7e6f0584449d3
Oliver Adams <oliver.adams@gmail.com>
2013-02-25 15:23:28 +1100

    Added Speaker class and corresponding tests

---
+++ Aikuma/src/au/edu/unimelb/aikuma/model/Speaker.java
@@ -0,0 +1,251 @@
+package au.edu.unimelb.aikuma.model;
+
+import android.graphics.Bitmap;
+import au.edu.unimelb.aikuma.util.FileIO;
+import au.edu.unimelb.aikuma.util.ImageUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * The class that stores the data about a speaker.
+ *
+ * @author	Oliver Adams	<oliver.adams@gmail.com>
+ * @author	Florian Hanke	<florian.hanke@gmail.com>
+ */
+public class Speaker {
+
+	/**
+	 * The minimal constructor; do not use if the Speaker already has a UUID,
+	 * as a new UUID will be generated.
+	 */
+	public Speaker() {
+		setUUID(UUID.randomUUID());
+		setLanguages(new ArrayList<Language>());
+	}
+
+	/**
+	 * The complete constructor
+	 */
+	public Speaker(UUID uuid, String name, List<Language> languages) {
+		setUUID(uuid);
+		setName(name);
+		setLanguages(languages);
+	}
+
+	/**
+	 * Gets the UUID of the speaker.
+	 *
+	 * @return	A UUID object.
+	 */
+	public UUID getUUID() {
+		return uuid;
+	}
+
+	/**
+	 * Gets the name of the Speaker.
+	 *
+	 * @return	A String object.
+	 */
+	public String getName() {
+		if (name == null) {
+			return "";
+		}
+		return name;
+	}
+
+	/**
+	 * Gets the list of languages associated with the Speaker.
+	 *
+	 * @return	A List of Language objects.
+	 */
+	public List<Language> getLanguages() {
+		return languages;
+	}
+
+	/**
+	 * Returns true if the Speaker has at least one language; false otherwise.
+	 *
+	 * @return	true if the Speaker has at least one language; false otherwise.
+	 */
+	public boolean hasALanguage() {
+		if (languages.size() == 0) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Gets the Speaker's image.
+	 *
+	 * @return	A Bitmap object.
+	 */
+	public Bitmap getImage() throws IOException {
+		return ImageUtils.retrieveFromFile(getImage() + ".jpg");
+	}
+
+	/**
+	 * Gets the small version of the Speaker's image.
+	 *
+	 * @return	A Bitmap object.
+	 */
+	public Bitmap getSmallImage() throws IOException {
+		return ImageUtils.retrieveFromFile(getSmallImage() + ".small.jpg");
+	}
+
+	/**
+	 * Encodes the Speaker object as a corresponding JSONObject.
+	 *
+	 * @return	A JSONObject instance representing the Speaker.
+	 */
+	public JSONObject encode() {
+		JSONObject encodedSpeaker = new JSONObject();
+		encodedSpeaker.put("name", this.name);
+		encodedSpeaker.put("uuid", this.uuid.toString());
+		encodedSpeaker.put("languages", Language.encodeList(languages));
+		return encodedSpeaker;
+	}
+
+	/**
+	 * Write the Speaker to file iin a subdirectory of the Speakers directory
+	 * named as <uuid>.json
+	 */
+	public void write() throws IOException {
+		JSONObject encodedSpeaker = this.encode();
+
+		FileIO.writeJSONObject(new File(getSpeakersPath(), getUUID() + 
+				"/metadata.json"), encodedSpeaker);
+	}
+
+	/**
+	 * read a Speaker from the file containing the JSON describing the speaker
+	 *
+	 * @param	uuid	A UUID object representing the UUID of the Speaker.
+	 */
+	public static Speaker read(UUID uuid) throws IOException {
+		JSONObject jsonObj = FileIO.readJSONObject(
+				new File(getSpeakersPath(), uuid + "/metadata.json"));
+		String uuidString = (String) jsonObj.get("uuid");
+		if (uuidString == null) {
+			throw new IOException("Null UUID in the JSON file.");
+		}
+		UUID readUUID = UUID.fromString(uuidString);
+		String name = (String) jsonObj.get("name");
+		JSONArray languageArray = (JSONArray) jsonObj.get("languages");
+		if (languageArray == null) {
+			throw new IOException("Null languages in the JSON file.");
+		}
+		List<Language> languages = Language.decodeJSONArray(languageArray);
+		return new Speaker(uuid, name, languages);
+	}
+
+	/**
+	 * Read all users from file
+	 *
+	 * @return	A list of the users found in the users directory.
+	 */
+	public static List<Speaker> readAll() {
+		// Get a list of all the UUIDs of users in the "users" directory.
+		List<String> speakerUUIDs = Arrays.asList(getSpeakersPath().list());
+
+		// Get the user data from the metadata.json files.
+		List<Speaker> speakers = new ArrayList<Speaker>();
+		for (String speakerUUID : speakerUUIDs) {
+			try {
+				speakers.add(Speaker.read(UUID.fromString(speakerUUID)));
+			} catch (IOException e) {
+				// Couldn't read that user for whatever reason (perhaps JSON
+				// file wasn't formatted correctly). Lets just ignore that user.
+			}
+		}
+		return speakers;
+	}
+
+	/**
+	 * Compares the given object with the Speaker, and returns true if the
+	 * Speaker uuid, name and languages are equal.
+	 *
+	 * @return	true if the uuid, name and languages of the Speaker are equal;
+	 * false otherwise.
+	 */
+	public boolean equals(Object obj) {
+		if (obj == null) { return false; }
+		if (obj == this) {return true; }
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+		Speaker rhs = (Speaker) obj;
+		return new EqualsBuilder()
+				.append(uuid, rhs.uuid).append(name, rhs.name)
+				.append(languages, rhs.languages).isEquals();
+	}
+
+	/**
+	 * Get the directory where the Speaker data is stored.
+	 *
+	 * @return	A file representing the path of the Speakers directory.
+	 */
+	 private static File getSpeakersPath() {
+	 	File path = new File(FileIO.getAppRootPath(), "speakers");
+		path.mkdirs();
+		return path;
+	 }
+
+	/**
+	 * Sets the UUID of the Speaker.
+	 *
+	 * @param	uuid	A UUID object representing the Speaker's UUID.
+	 */
+	private void setUUID(UUID uuid) throws IllegalArgumentException {
+		if (uuid == null) {
+			throw new IllegalArgumentException("Speaker UUID cannot be null.");
+		}
+		this.uuid = uuid;
+	}
+
+	/**
+	 * Sets the name of the Speaker.
+	 *
+	 * @param	name	A String object representing the Speaker's name.
+	 */
+	private void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Sets the Languages of the Speaker.
+	 *
+	 * @param	languages	A List<Language> object representing the languages
+	 * associated with the Speaker.
+	 */
+	private void setLanguages(List<Language> languages) throws
+			IllegalArgumentException {
+		if (languages == null) {
+			throw new IllegalArgumentException("Speaker languages cannot be null.");
+		}
+		this.languages = languages;
+	}
+
+
+	/**
+	 * The UUID of the Speaker.
+	 */
+	private UUID uuid;
+
+	/**
+	 * The name of the Speaker.
+	 */
+	private String name;
+
+	/**
+	 * The languages of the Speaker.
+	 */
+	private List<Language> languages;
+
+}
---
+++ Aikuma/test/src/au/edu/unimelb/aikuma/model/SpeakerTest.java
@@ -0,0 +1,204 @@
+package au.edu.unimelb.aikuma.model;
+
+import android.test.AndroidTestCase;
+import au.edu.unimelb.aikuma.util.FileIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Tests for model.Speaker, the class that represents the speaker of a
+ * Recording.
+ */
+
+public class SpeakerTest extends AndroidTestCase {
+
+	/**
+	 * Tests the minimal constructor and accessors.
+	 */
+	public void testConstruction1() {
+		Speaker speaker = new Speaker();
+		assertTrue(speaker.getUUID() != null);
+		assertEquals("", speaker.getName());
+		assertEquals(new ArrayList<Language>(), speaker.getLanguages());
+		assertTrue(!speaker.hasALanguage());
+	}
+
+	/**
+	 * Tests the complete constructor and accessors.
+	 */
+	public void testConstruction2() {
+		UUID uuid = UUID.randomUUID();
+		String name = "Some silly name";
+		Language usarufa = new Language("Usarufa", "usa");
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(usarufa);
+		Speaker speaker = new Speaker(uuid, name, languages);
+
+		assertEquals(uuid, speaker.getUUID());
+		assertEquals(name, speaker.getName());
+		assertEquals(languages, speaker.getLanguages());
+		assertTrue(speaker.hasALanguage());
+	}
+
+	/**
+	 * Tests Speaker encoding.
+	 */
+	public void testEncode() {
+		Language usarufa = new Language("Usarufa", "usa");
+		Language english = new Language("English", "eng");
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(usarufa);
+		languages.add(english);
+		Speaker speaker = new Speaker(UUID.randomUUID(), "ASpeaker", languages);
+		assertEquals(
+				"{\"languages\":[{\"code\":\"usa\",\"name\":\"Usarufa\"}," +
+				"{\"code\":\"eng\",\"name\":\"English\"}],\"uuid\":\"" +
+				speaker.getUUID() + "\",\"name\":\"ASpeaker\"}",
+				speaker.encode().toString());
+	}
+
+	/**
+	 * Tests reading a nonexistent Speaker's JSON file.
+	 */
+	public void testRead1() {
+		boolean caught = false;
+		try {
+			Speaker.read(UUID.randomUUID());
+		} catch (IOException e) {
+			caught = true;
+		}
+		assertTrue(caught);
+	}
+
+	public void testRead2() throws IOException {
+		UUID uuid = UUID.randomUUID();
+		String name = "TestSpeaker";
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(new Language("Usarufa", "usa"));
+		Speaker speaker = new Speaker(uuid, name, languages);
+		String jsonStr = "{\"uuid\":\"" + uuid + "\",\"name\":\"TestSpeaker\"" +
+				"\"languages\":[{\"code\":\"usa\",\"name\":\"Usarufa\"}]}";
+		File file = new File(FileIO.getAppRootPath(), "speakers/" + uuid +
+				"/metadata.json");
+		FileIO.write(file, jsonStr);
+		Speaker readSpeaker = Speaker.read(uuid);
+		assertEquals(speaker.getUUID(), readSpeaker.getUUID());
+		assertEquals(speaker.getName(), readSpeaker.getName());
+		assertEquals(speaker.getLanguages(), readSpeaker.getLanguages());
+		FileUtils.deleteDirectory(file.getParentFile());
+	}
+
+	public void testRead3() throws IOException {
+		UUID uuid = UUID.randomUUID();
+		String name = null;
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(new Language("Usarufa", "usa"));
+		Speaker speaker = new Speaker(uuid, name, languages);
+		String jsonStr = "{\"uuid\":\"" + uuid + "\",\"name\":null" +
+				"\"languages\":[{\"code\":\"usa\",\"name\":\"Usarufa\"}]}";
+		File file = new File(FileIO.getAppRootPath(), "speakers/" + uuid +
+				"/metadata.json");
+		FileIO.write(file, jsonStr);
+		Speaker readSpeaker;
+		readSpeaker = Speaker.read(uuid);
+		assertEquals(speaker.getUUID(), readSpeaker.getUUID());
+		assertEquals(speaker.getName(), readSpeaker.getName());
+		assertEquals(speaker.getLanguages(), readSpeaker.getLanguages());
+		FileUtils.deleteDirectory(file.getParentFile());
+	}
+
+	public void testRead4() throws IOException {
+		UUID uuid = UUID.randomUUID();
+		String name = null;
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(new Language("Usarufa", "usa"));
+		Speaker speaker = new Speaker(uuid, name, languages);
+		String jsonStr = "{\"languages\":[{\"code\":\"usa\",\"name\":\"Usarufa\"}]}";
+		File file = new File(FileIO.getAppRootPath(), "speakers/" + uuid +
+				"/metadata.json");
+		FileIO.write(file, jsonStr);
+		boolean caught = false;
+		try {
+			Speaker readSpeaker = Speaker.read(uuid);
+		} catch (IOException e) {
+			caught = true;
+			assertEquals("Null UUID in the JSON file.", e.getMessage());
+		}
+		assertTrue(caught);
+		FileUtils.deleteDirectory(file.getParentFile());
+	}
+
+	public void testRead5() throws IOException {
+		UUID uuid = UUID.randomUUID();
+		String name = null;
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(new Language("Usarufa", "usa"));
+		Speaker speaker = new Speaker(uuid, name, languages);
+		String jsonStr = "{\"uuid\":\"" + uuid + "\",\"name\":null" +
+				"\"languages\":null}";
+		File file = new File(FileIO.getAppRootPath(), "speakers/" + uuid +
+				"/metadata.json");
+		FileIO.write(file, jsonStr);
+		boolean caught = false;
+		try {
+			Speaker readSpeaker = Speaker.read(uuid);
+		} catch (IOException e) {
+			caught = true;
+			assertEquals("Null languages in the JSON file.", e.getMessage());
+		}
+		assertTrue(caught);
+		FileUtils.deleteDirectory(file.getParentFile());
+	}
+
+	public void testWriteRead() throws IOException {
+		UUID uuid = UUID.randomUUID();
+		String name = "TestSpeaker";
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(new Language("Usarufa", "usa"));
+		Speaker speaker = new Speaker(uuid, name, languages);
+		speaker.write();
+		Speaker readSpeaker = Speaker.read(speaker.getUUID());
+		assertEquals(speaker, readSpeaker);
+	}
+
+	/**
+	 * Ensure readAll functions when there is an invalid Speaker JSON file amongst
+	 * valid Speaker JSON files - No exception should be thrown, but the invalid
+	 * Speaker should not be read.
+	 */
+	public void testReadAll() throws IOException {
+		// Write an inappropriate metadata file (no UUID in the file)
+		UUID uuid = UUID.randomUUID();
+		String jsonStr = "{\"name\":\"TestSpeaker\"}";
+		File file = new File(FileIO.getAppRootPath(),
+				"speakers/" + uuid + "/metadata.json");
+		FileIO.write(file, jsonStr);
+		// Write a user with a name and uuid
+		Speaker speaker1 = new Speaker(UUID.randomUUID(), "TestSpeaker1", new
+				ArrayList<Language>());
+		speaker1.write();
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(new Language("Usarufa", "usa"));
+		// Write a user with a name, uuid, and language.
+		Speaker speaker2 = new Speaker(UUID.randomUUID(), "TestSpeaker1",
+				languages);
+		speaker2.write();
+
+		List<Speaker> speakerList = Speaker.readAll();
+		// Ensure the other two are actually the corresponding users.
+		assertTrue(speakerList.contains(speaker1));
+		assertTrue(speakerList.contains(speaker2));
+
+		FileUtils.deleteDirectory(new File(FileIO.getAppRootPath(), "speakers/" + 
+				uuid + "/metadata.json").getParentFile());
+		FileUtils.deleteDirectory(new File(FileIO.getAppRootPath(), "speakers/" + 
+				speaker1.getUUID() + "/metadata.json").getParentFile());
+		FileUtils.deleteDirectory(new File(FileIO.getAppRootPath(), "speakers/" + 
+				speaker2.getUUID() + "/metadata.json").getParentFile());
+
+	}
+}
