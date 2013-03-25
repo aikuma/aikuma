bb927ac176c13a246788a40aab1c46ad3c499cac
Florian René Hanke <florian.hanke@gmail.com>
2013-03-25 11:12:21 +1100

    Extract audio average processing into its own class for reuse with background noise extraction.

---
+++ BOLDApp/src/au/edu/unimelb/boldapp/audio/Processor.java
@@ -0,0 +1,30 @@
+package au.edu.unimelb.aikuma.audio;
+
+/** 
+ * Can do basic processing on audio buffers.
+ *
+ * @author	Florian Hanke	<florian.hanke@gmail.com>
+ * @author	Oliver Adams	<oliver.adams@gmail.com>
+ */
+public class Processor {
+  
+  /** Evaluates the average amplitude.
+   *
+   * @return Average amplitude of the buffer.
+   */
+	public int getAverage(short[] buffer) {
+		int sum = 0;
+		int amount = 0;
+
+		for (int i = 0; i < buffer.length; i++) {
+			short value = buffer[i];
+			
+			if (value >= 0) { 
+				sum += value;
+				amount += 1;
+			}
+		}
+
+		return amount == 0 ? sum : sum / amount;
+	}
+}
