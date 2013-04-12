ea4459dde09de83920515ae681b403463ca4f480
Oliver Adams <oliver.adams@gmail.com>
2013-04-12 11:45:28 +1000

    including a test for 16000 and for 44100 hz sample rates

--- Aikuma/test/src/org/lp20/aikuma/audio/record/RecorderTest.java
+++ Aikuma/test/src/org/lp20/aikuma/audio/record/RecorderTest.java
@@ -14,11 +14,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class RecorderTest extends AndroidTestCase {
 	public void testRecorder() throws Exception {
+		recorderTest(16000);
+		recorderTest(44100);
+	}
+
+	public void recorderTest(int sampleRate) throws Exception {
 		File f = new File("/mnt/sdcard/aikuma/testrecordings/testrecord1.wav");
-		Recorder recorder = new Recorder(f, 16000);
+		Recorder recorder = new Recorder(f, sampleRate);
 		//recorder.prepare(path);
 		recorder.listen();
-		Log.i("ManualTesting", "Recording started.");
+		Log.i("ManualTesting", "Recording started at " + sampleRate + ".");
 		TimeUnit.SECONDS.sleep(10);
 		recorder.stop();
 		Log.i("ManualTesting", "Recording stopped.");
@@ -27,7 +32,7 @@ public class RecorderTest extends AndroidTestCase {
 		mediaPlayer.setDataSource(f.getPath());
 		mediaPlayer.prepare();
 		mediaPlayer.start();
-		Log.i("ManualTesting", "Playback started.");
+		Log.i("ManualTesting", "Playback started at " + sampleRate + ".");
 		TimeUnit.SECONDS.sleep(10);
 	}
 }
