856ab75e9589fe5bddb5f4a4da45ab4b2361239c
Oliver Adams <oliver.adams@gmail.com>
2013-04-26 17:08:10 +1000

    removed Log.i statements

--- Aikuma/test/src/org/lp20/aikuma/aikuma/AikumaTest.java
+++ Aikuma/test/src/org/lp20/aikuma/aikuma/AikumaTest.java
@@ -6,7 +6,6 @@ import android.util.Log;
 public class AikumaTest extends AndroidTestCase {
 
 	public void testGetAndroidID() {
-		Log.i("GetAndroidIDTest", Aikuma.getAndroidID());
 		assertTrue(Aikuma.getAndroidID() != null);
 	}
 }
