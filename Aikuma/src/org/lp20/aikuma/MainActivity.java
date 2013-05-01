20a787c060fdd644db86dcc341ee48e05ef48e0f
Oliver Adams <oliver.adams@gmail.com>
2013-05-01 14:02:17 +1000

    working on the design of the main screen. Currently have the sync, search and settings icons on the ActionBar, with the record button awkwardly located

--- Aikuma/src/org/lp20/aikuma/MainActivity.java
+++ Aikuma/src/org/lp20/aikuma/MainActivity.java
@@ -1,15 +1,28 @@
 package org.lp20.aikuma;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		//ActionBar actionBar = getActionBar();
+		//actionBar.setDisplayShowHomeEnabled(false);
+		//actionBar.setDisplayShowTitleEnabled(false);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
 }
