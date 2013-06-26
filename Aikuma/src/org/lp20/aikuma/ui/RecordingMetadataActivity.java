6fb525b200de406e08a0cc60173482144e6cef19
Oliver Adams <oliver.adams@gmail.com>
2013-06-26 14:24:54 +1000

    Added a confirmation dialog boxes to the RecordingMetadataActivity

--- Aikuma/src/org/lp20/aikuma/ui/RecordingMetadataActivity.java
+++ Aikuma/src/org/lp20/aikuma/ui/RecordingMetadataActivity.java
@@ -1,13 +1,18 @@
 package org.lp20.aikuma.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import org.lp20.aikuma.R;
+import org.lp20.aikuma.MainActivity;
 
 /**
  * The activity that allows audio to be recorded
@@ -29,5 +34,42 @@ public class RecordingMetadataActivity extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void onOkButtonPressed(View view) {
+		new AlertDialog.Builder(this)
+				.setMessage(R.string.share_dialog)
+				.setPositiveButton(R.string.share, new
+				DialogInterface.OnClickListener() {
+				
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Intent intent =
+								new Intent(RecordingMetadataActivity.this,
+										MainActivity.class);
+						intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						startActivity(intent);
+					}
+				})
+				.setNegativeButton(R.string.cancel, null)
+				.show();
+	}
+	public void onCancelButtonPressed(View view) {
+		new AlertDialog.Builder(this)
+				.setMessage(R.string.discard_dialog)
+				.setPositiveButton(R.string.discard, new
+				DialogInterface.OnClickListener() {
+				
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Intent intent =
+								new Intent(RecordingMetadataActivity.this,
+										MainActivity.class);
+						intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						startActivity(intent);
+					}
+				})
+				.setNegativeButton(R.string.cancel, null)
+				.show();
 	}
 }
