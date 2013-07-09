ebeb71286d9e51205e0c959849109f2c11fa4522
Oliver Adams <oliver.adams@gmail.com>
2013-07-09 13:14:27 +1000

    added stub code for the AddSpeakerActivity

--- Aikuma/src/org/lp20/aikuma/ui/AddSpeakerActivity.java
+++ Aikuma/src/org/lp20/aikuma/ui/AddSpeakerActivity.java
@@ -6,17 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import org.lp20.aikuma.R;
 
-public class SpeakersActivity extends ListActivity {
+public class AddSpeakerActivity extends ListActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.speakers);
-	}
-
-	public void addSpeakerButtonPressed(View view) {
-		Intent intent = new Intent(this, AddSpeakerActivity.class);
-		startActivity(intent);
+		setContentView(R.layout.add_speaker);
 	}
 
 }
