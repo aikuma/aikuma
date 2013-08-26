346a1bfb134c50fb463341d5ae9556994bd7997a
Oliver Adams <oliver.adams@gmail.com>
2013-08-26 15:50:18 +1000

    made a commit of the sync stuff that has been happening recently. Most of the commits have been to a library Ive kept separate from this repository, FTPSyncUtil. This snapshot wont work because sync.jar (which has org.lp20.sync in it) is not included. Because of time constraints Im going to use the old sync code.

--- Aikuma/src/org/lp20/aikuma/util/SyncUtil.java
+++ Aikuma/src/org/lp20/aikuma/util/SyncUtil.java
@@ -1,8 +1,11 @@
 package org.lp20.aikuma.util;
 
 import android.util.Log;
+import android.widget.Toast;
+import java.io.File;
 import java.io.IOException;
 import org.lp20.aikuma.model.ServerCredentials;
+import org.lp20.sync.FTPSyncUtil;
 
 public class SyncUtil {
 
@@ -14,12 +17,25 @@ public class SyncUtil {
 
 	private class SyncLoop extends Thread {
 		public void run() {
-			Log.i("sync", "hi");
+			FTPSyncUtil ftpSyncUtil = new FTPSyncUtil();
+			//client.setClientBaseDir(FileIO.getAppRootPath().toString());
 			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
+				ftpSyncUtil.connect("192.168.1.1");
+				ftpSyncUtil.login("admin", "admin");
+				ftpSyncUtil.setClientSyncDir(FileIO.getAppRootPath());
+				Log.i("sync", "findwritabledir: " +
+				ftpSyncUtil.findWritableServerDir(new File("/")));
+				ftpSyncUtil.setServerSyncDir(
+						ftpSyncUtil.findWritableServerDir(new File("/")));
+				Log.i("sync", "serverSyncDir: " +
+						ftpSyncUtil.getServerSyncDir());
+				Log.i("sync", "sync success: " + ftpSyncUtil.sync());
+				ftpSyncUtil.logout();
+				ftpSyncUtil.disconnect();
+			} catch (IOException e) {
+				Log.i("sync", "exception thrown: " + e.getMessage());
 			}
-			Log.i("sync", "bye");
+			Log.i("sync", "sync complete");
 		}
 	}
 
