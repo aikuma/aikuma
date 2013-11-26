e7f69cc3e876788fdcf38f7eae42a625f24a5d7f
Oliver Adams <oliver.adams@gmail.com>
2013-11-26 14:35:59 +1100

    Updated AndroidManifest for latest version - beta 10

---
+++ sign.sh
@@ -0,0 +1,7 @@
+cd Aikuma
+pwd
+ant release
+pwd
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../aikuma.keystore bin/Aikuma-release-unsigned.apk aikuma_key
+pwd
+zipalign -v 4 bin/Aikuma-release-unsigned.apk bin/Aikuma.apk
