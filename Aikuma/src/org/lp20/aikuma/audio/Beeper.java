639386328527ec3f2151054046f60bbebd8b54cc
Oliver Adams <oliver.adams@gmail.com>
2014-01-23 14:19:20 +1100

    Finished javadocs

--- Aikuma/src/org/lp20/aikuma/audio/Beeper.java
+++ Aikuma/src/org/lp20/aikuma/audio/Beeper.java
@@ -26,31 +26,69 @@ public class Beeper {
 	private MediaPlayer beep;
 	private MediaPlayer beepBeep;
 	
+	/**
+	 * Constructor
+	 *
+	 * @param	context	The context that the Beeper should beep in.
+	 */
 	public Beeper(Context context) {
 		this.context = context;
 	}
 
+	/**
+	 * Plays one beep.
+	 */
 	public void beep() { beep(null); }
+
+	/**
+	 * Plays one beep.
+	 *
+	 * @param	listener	The callback to play when the beep is complete.
+	 */
 	public void beep(OnCompletionListener listener) {
 		getBeep(listener).start();
 	}
-	
+
+	/**
+	 * Plays two beeps in succession.
+	 */
 	public void beepBeep() { beepBeep(null); }
+
+	/**
+	 * Plays two beeps in succession.
+	 *
+	 * @param	listener	The callback to play when the beep is complete.
+	 */
 	public void beepBeep(OnCompletionListener listener) {
 		getBeepBeep(listener).start();
 	}
-	
-	/** Static convenience methods */
-	
+
+	/**
+	 * Plays one beep.
+	 *
+	 * @param	context	The context to play the beep in
+	 * @param	listener	The callback to play when the beep is complete.
+	 */
 	public static void beep(Context context, OnCompletionListener listener) {
 		start(context, listener, R.raw.beep);
 	}
 
-	
+	/**
+	 * Plays two beeps in succession.
+	 *
+	 * @param	context	The context to play the beep in
+	 * @param	listener	The callback to play when the beep is complete.
+	 */
 	public static void beepBeep(Context context, OnCompletionListener listener) {
 		start(context, listener, R.raw.beeps2);
 	}
 
+	/**
+	 * Plays a long beep.
+	 *
+	 * @param	context	The context to play the beep in
+	 * @param	listener	The callback to play when the beep is complete.
+	 */
 	public static void longBeep(Context context, OnCompletionListener listener) {
 		start(context, listener, R.raw.longbeep);
 	}
