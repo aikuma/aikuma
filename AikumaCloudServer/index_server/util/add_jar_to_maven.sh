43a548e08384c848516ad4ac2e98d50cc803d8f1
Robert Parker <parkerrl@ldc.upenn.edu>
2014-10-28 15:54:30 -0400

    Initial commit
    Committing work on index server

---
+++ AikumaCloudServer/index_server/util/add_jar_to_maven.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=../lib/../lib/AikumaCloudStorage-with_dependencies.jar \
+     -DgroupId=org.lp20.aikuma -DartifactId=cloud-storage-with-deps \
+     -Dversion=1.0 -Dpackaging=jar
