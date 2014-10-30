fc642e876c9d33117a4f831fc678f4e892ab3f78
Robert Parker <parkerrl@ldc.upenn.edu>
2014-10-30 15:58:14 -0400

    Fix path weirdness
    For some reason, the original script wound up with a duplicate ../lib
    prepended to the source information. fixed.

--- AikumaCloudServer/index_server/util/add_jar_to_maven.sh
+++ AikumaCloudServer/index_server/util/add_jar_to_maven.sh
@@ -1,3 +1,3 @@
-mvn install:install-file -Dfile=../lib/../lib/AikumaCloudStorage-with_dependencies.jar \
+mvn install:install-file -Dfile=../lib/AikumaCloudStorage-with_dependencies.jar \
      -DgroupId=org.lp20.aikuma -DartifactId=cloud-storage-with-deps \
      -Dversion=1.0 -Dpackaging=jar
