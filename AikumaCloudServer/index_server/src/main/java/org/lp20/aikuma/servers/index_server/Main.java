09f11d169c4986e35b920b93a38f960399c091bd
Robert Parker <parkerrl@ldc.upenn.edu>
2014-11-02 23:36:45 -0500

    SSL support, rename props file
    - Added SSL support to server
    - Made main method not exit
    - Renamed fusion_index.properties -> index_server.properties
    - Added new properties to the config to support SSL

--- AikumaCloudServer/index_server/src/main/java/org/lp20/aikuma/servers/index_server/Main.java
+++ AikumaCloudServer/index_server/src/main/java/org/lp20/aikuma/servers/index_server/Main.java
@@ -1,10 +1,20 @@
 package org.lp20.aikuma.servers.index_server;
 
+import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ContainerFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.ws.rs.core.Application;
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -16,25 +26,23 @@ import java.util.logging.Logger;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/";
-
-
     private static final Logger log = Logger.getLogger(Main.class.getName());
-    private static String accessToken;
-    private static String refreshToken;
-    private static String tableId;
 
     private static boolean setup(IndexServerApplication app) {
         Properties props = new Properties();
         String fileLoc = System.getProperty("user.dir");
         fileLoc += System.getProperty("file.separator");
-        fileLoc += "fusion_index.properties";
+        fileLoc += "index_server.properties";
         try {
             InputStream in = new FileInputStream(fileLoc);
             props.load(in);
             in.close();
             Map<String,Object> tmp = new HashMap<String, Object>(5);
             tmp.put("table_id", props.getProperty("table_id"));
+            tmp.put("base_uri", props.getProperty("base_uri"));
+            tmp.put("use_ssl", props.getProperty("use_ssl"));
+            tmp.put("keystore_file", props.getProperty("keystore_file"));
+            tmp.put("keystore_password", props.getProperty("keystore_password"));
 
             app.tokenManager = new TokenManager(props.getProperty("client_id"),
                                                 props.getProperty("client_secret"),
@@ -67,7 +75,30 @@ public class Main {
         }
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), app);
+        HttpServer server;
+        URI base_uri = URI.create((String) app.getProperty("base_uri"));
+
+        if (((String) app.getProperty("use_ssl")).equals("true")) {
+            SSLContextConfigurator scc = new SSLContextConfigurator();
+            URL url = null;
+            try {
+                url = new URL((String) app.getProperty("keystore_file"));
+            } catch (MalformedURLException e) {
+                log.severe("Unable to find keystore file; can't continue");
+                System.exit(1);
+            }
+            scc.setKeyStoreFile(url.getFile());
+            scc.setKeyStorePass((String) app.getProperty("keystore_password"));
+
+             server = GrizzlyHttpServerFactory.createHttpServer(base_uri,
+                    app,
+                    true,
+                    new SSLEngineConfigurator(scc.createSSLContext(), false, false, false));
+        } else {
+             server = GrizzlyHttpServerFactory.createHttpServer(base_uri, app);
+        }
+        log.info("Starting server at " + base_uri + "; ssl enabled = " + app.getProperty("use_ssl"));
+        return server;
     }
 
     /**
@@ -76,12 +107,18 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-
+        long startTime = System.currentTimeMillis();
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.shutdownNow();
+        long startup = System.currentTimeMillis() - startTime;
+
+        System.out.println("Application started in " + startup + " milliseconds");
+        while (true) {
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                server.shutdownNow();
+            }
+        }
     }
 
 
