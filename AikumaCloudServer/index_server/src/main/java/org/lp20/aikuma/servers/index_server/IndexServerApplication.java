3b90828ec0cdbd326ae99a7e560bd377a7712869
Robert Parker <parkerrl@ldc.upenn.edu>
2014-11-14 17:08:07 -0500

    Implement client auth and JWT service auth
    In retrospect, I probably should've broken this into at least two
    commits, but I didn't and so here we are.
    
    - Clients can now be required to pass a Google signed JWT auth token to
      the service to make queries (configurable in the config file)
      NB: I've only figured out how to get this token on the client side
      from an Android app; I'll add the sample code I wrote to test this
      to git after this commit.
    
    - The server now uses a service account to access Google APIs (Fusion
      Tables at this point). This eliminates the need for manually
      generating the auth and refresh tokens to put in the config file. It's
      also how server processes are meant to interact with Google APIs, so
      that's nice.

--- AikumaCloudServer/index_server/src/main/java/org/lp20/aikuma/servers/index_server/IndexServerApplication.java
+++ AikumaCloudServer/index_server/src/main/java/org/lp20/aikuma/servers/index_server/IndexServerApplication.java
@@ -6,6 +6,9 @@ import org.glassfish.jersey.server.ResourceConfig;
  * Created by bob on 10/30/14.
  */
 public class IndexServerApplication extends ResourceConfig{
+    public IndexServerApplication() {
+
+    }
     public TokenManager tokenManager;
     public JWTVerifier jwtVerifier = null;
 
