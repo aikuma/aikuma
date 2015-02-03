010db694e4aae86e5a528f623132aed5233a39fc
Haejoong Lee <haejoong@ldc.upenn.edu>
2015-02-03 02:33:40 -0500

    index server v1.0.1

--- AikumaCloudServer/index_server/pkg/build.sh
+++ AikumaCloudServer/index_server/pkg/build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 REPO=hleeldc/aikuma-index-server
-VER=1.0.0
+VER=1.0.1
 TAG=$REPO:$VER
 TAG_LATEST=$REPO:latest
 
 docker build -t $REPO:$VER .
-docker tag $TAG $TAG_LATEST
+docker tag -f $TAG $TAG_LATEST
 
