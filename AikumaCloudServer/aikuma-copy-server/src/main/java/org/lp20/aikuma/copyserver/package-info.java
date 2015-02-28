29dff0b49f39139a757b2adc74c65e1f5d48f3bd
Haejoong Lee <haejoong@ldc.upenn.edu>
2015-02-28 13:17:20 -0500

    keep cloud storage library in a separate branch

--- AikumaCloudServer/aikuma-copy-server/src/main/java/org/lp20/aikuma/copyserver/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author haejoong
- *
- */
-package org.lp20.aikuma.copyserver;
--- AikumaCloudServer/index_server/util/add_jar_to_maven.sh
+++
@@ -1,3 +0,0 @@
-mvn install:install-file -Dfile=../lib/AikumaCloudStorage-with_dependencies-1.1.jar \
-     -DgroupId=org.lp20.aikuma -DartifactId=cloud-storage-with-deps \
-     -Dversion=1.1 -Dpackaging=jar
