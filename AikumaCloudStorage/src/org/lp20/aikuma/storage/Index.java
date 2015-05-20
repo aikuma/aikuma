6e4932cb0616ab4635fe7c460e1ad600cb608045
Haejoong Lee <haejoong@ldc.upenn.edu>
2015-05-20 12:14:32 -0400

    fix indentation

--- AikumaCloudStorage/src/org/lp20/aikuma/storage/Index.java
+++ AikumaCloudStorage/src/org/lp20/aikuma/storage/Index.java
@@ -5,42 +5,42 @@ import java.util.Map;
 
 public interface Index {
 
-	/**
-	 * Downloads metadata for the item.
-	 * 
-	 * @param identifier
-	 * @return The metadata for the item as a key-value pairs. Null if item is
-	 *   not found in the index.
-	 */
-	public abstract Map<String, String> getItemMetadata(String identifier);
+    /**
+     * Downloads metadata for the item.
+     * 
+     * @param identifier
+     * @return The metadata for the item as a key-value pairs. Null if item is
+     *   not found in the index.
+     */
+    public abstract Map<String, String> getItemMetadata(String identifier);
 
-	/**
-	 * Search the index according to the specified constraints.
-	 * TODO: Specify the structure of the constraints param.
-	 * @param constraints
-	 * @return A list of item ids.
-	 */
-	public abstract List<String> search(Map<String,String> constraints);
+    /**
+     * Search the index according to the specified constraints.
+     * TODO: Specify the structure of the constraints param.
+     * @param constraints
+     * @return A list of item ids.
+     */
+    public abstract List<String> search(Map<String,String> constraints);
 
     public abstract void search(Map<String, String> constraints, SearchResultProcessor processor);
 
-	/**
-	 * Index an item. If item already exists, it gets updated.
-	 * For metadata, the following keys are required:
-	 * 
-	 *   - data_store_uri - the Google Drive URI
-	 *   - item_id        - A way of grouping multiple items under a single identifier
-	 *   - file_type      - Mime type
-	 *   - language       - language
-	 *   - speakers (comma-separated list) - speakers
-	 * 
-	 * @param identifier
-	 * @param metadata
-	 */
+    /**
+     * Index an item. If item already exists, it gets updated.
+     * For metadata, the following keys are required:
+     * 
+     *   - data_store_uri - the Google Drive URI
+     *   - item_id        - A way of grouping multiple items under a single identifier
+     *   - file_type      - Mime type
+     *   - language       - language
+     *   - speakers (comma-separated list) - speakers
+     * 
+     * @param identifier
+     * @param metadata
+     */
 
 
 
-	public abstract boolean index(String identifier, Map<String,String> metadata);
+    public abstract boolean index(String identifier, Map<String,String> metadata);
 
 
     /**
